cabe4cf7725b9f4817131689175c6eecbdca3453
Ovgodd <c.gromoff@gmail.com>
2023-01-10 23:22:30 +0100

    🚧 F01 LOGIN > Add  Break line

--- src/Components/Pages/LoginPage.js
+++ src/Components/Pages/LoginPage.js
@@ -18,6 +18,7 @@ export default function LoginPage() {
   return (
     <div>
       <h1>Bienvenue chez nous</h1>
+      <br />
       <h2>Connectez-vous</h2>
       <form action="submit" onSubmit={handleSubmit}>
         <input
