46b1f408b9b4e7fd49eb5c0e9345b93d487aa044
Ovgodd <c.gromoff@gmail.com>
2023-01-10 23:58:10 +0100

    ♻️ F01: LOGIN > Create LoginForm

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import LoginPage from "./Components/Pages/LoginPage";
+import LoginPage from "./components/pages/login/LoginPage";
 
 function App() {
   // affichage(render)
---
+++ src/components/pages/login/LoginForm.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+export default function LoginForm() {
+  // state(état, données)
+  const [name, setName] = useState("");
+  // comportements
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert("bonjour " + name);
+    console.log("nom apres saisi " + name);
+    setName("");
+  };
+  const handleChange = (event) => {
+    setName(event.target.value);
+    console.log(name);
+  };
+
+  // affichage
+  return (
+    <form action="submit" onSubmit={handleSubmit}>
+      <h1>Bienvenue chez nous</h1>
+      <br />
+      <h2>Connectez-vous</h2>
+      <input
+        type="text"
+        value={name}
+        placeholder="Entrez votre prénom..."
+        onChange={handleChange}
+        required
+      />
+      <button>Accédez à votre espace</button>
+    </form>
+  );
+}
