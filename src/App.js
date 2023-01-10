35c3aeae84b948905c11e4aa7408c2335533b7ee
Ovgodd <c.gromoff@gmail.com>
2023-01-10 22:54:12 +0100

    ✨ F01: LOGIN > Create LoginPage add all code

--- src/App.js
+++ src/App.js
@@ -1,38 +1,8 @@
-import { useState } from "react";
+import LoginPage from "./Components/Pages/LoginPage";
 
 function App() {
-  // state(état, données)
-  const [name, setName] = useState("");
-  // comportements
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    alert("bonjour " + name);
-    console.log("nom apres saisie " + name);
-
-    setName("");
-  };
-  const handleChange = (event) => {
-    setName(event.target.value);
-    console.log(name);
-  };
   // affichage(render)
-  return (
-    //<LoginForm />
-    <div>
-      <h1>Bienvenue chez nous</h1>
-      <h2>Connectez-vous</h2>
-      <form action="submit" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={name}
-          placeholder="Entrez votre prénom..."
-          onChange={handleChange}
-          required
-        />
-        <button>Accédez à votre espace</button>
-      </form>
-    </div>
-  );
+  return <LoginPage />;
 }
 
 export default App;
---
+++ src/Components/Pages/LoginPage.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+export default function LoginPage() {
+  // state(état, données)
+  const [name, setName] = useState("");
+  // comportements
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert("bonjour " + name);
+    console.log("nom apres saisie " + name);
+
+    setName("");
+  };
+  const handleChange = (event) => {
+    setName(event.target.value);
+    console.log(name);
+  };
+  return (
+    <div>
+      <h1>Bienvenue chez nous</h1>
+      <h2>Connectez-vous</h2>
+      <form action="submit" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={name}
+          placeholder="Entrez votre prénom..."
+          onChange={handleChange}
+          required
+        />
+        <button>Accédez à votre espace</button>
+      </form>
+    </div>
+  );
+}
