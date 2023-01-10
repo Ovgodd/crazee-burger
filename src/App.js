ad4fbc150459ef362d68e97e3500ab477f7f964d
Ovgodd <c.gromoff@gmail.com>
2023-01-10 22:36:09 +0100

    🚧 F01: Wip App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { useState } from "react";
 
 function App() {
+  // state(état, données)
+  const [name, setName] = useState("");
+  // comportements
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert("bonjour " + name);
+    console.log("nom apres saisie " + name);
+
+    setName("");
+  };
+  const handleChange = (event) => {
+    setName(event.target.value);
+    console.log(name);
+  };
+  // affichage(render)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Salut Cyril</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //<LoginForm />
+    <div>
+      <h1>Bienvenue chez nous</h1>
+      <h2>Connectez-vous</h2>
+      <form action="submit" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={name}
+          placeholder="Entrez votre prénom..."
+          onChange={handleChange}
+          required
+        />
+        <button>Accédez à votre espace</button>
+      </form>
     </div>
   );
 }
