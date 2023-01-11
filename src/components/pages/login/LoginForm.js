a6f5d2fe03909f22752364e849c5641d4b057802
Ovgodd <c.gromoff@gmail.com>
2023-01-11 16:50:34 +0100

    ♻️ F01: LOGIN > simplify handleChange

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -1,19 +1,20 @@
 import React, { useState } from "react";
+import { isValidName } from "./utils";
 
 export default function LoginForm() {
   // state(état, données)
   const [name, setName] = useState("");
+
   // comportements
   const handleSubmit = (event) => {
     event.preventDefault();
     alert("bonjour " + name);
     setName("");
   };
+
   const handleChange = (event) => {
-    const regex = /^[A-Za-z]+$/;
-    if (event.target.value === "" || regex.test(event.target.value)) {
-      setName(event.target.value);
-    }
+    const inputUser = event.target.value;
+    if (inputUser === "" || isValidName.test(inputUser)) setName(inputUser);
   };
 
   // affichage
---
+++ src/components/pages/login/utils.js
@@ -0,0 +1,1 @@
+export const isValidName = /^[A-Za-z]+$/;
