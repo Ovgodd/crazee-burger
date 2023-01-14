a51053d8493ab62df919da935ffa0d9e77bc7ea1
Ovgodd <c.gromoff@gmail.com>
2023-01-14 10:56:30 +0100

    🔥 F03: LoginFrom.js > Delete not used import link

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { isValidName } from "./utils";
 export default function LoginForm() {
   // state(état, données)
