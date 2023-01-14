533d76e8cf37443057d9025c2fe2473dfdc34c1d
Ovgodd <c.gromoff@gmail.com>
2023-01-14 18:07:04 +0100

    ✨ F03: LOGIN > add logo and background code

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { isValidName } from "./utils";
 import styled from "styled-components";
+import { theme } from "../../../theme";
+import Logo from "../../../images/F03 logo-orange.png";
 
 export default function LoginForm() {
   // state(état, données)
@@ -25,22 +27,73 @@ export default function LoginForm() {
 
   return (
     <LoginFormStyled action="submit" onSubmit={handleSubmit}>
-      <h1>Bienvenue chez nous</h1>
-      <br />
-      <h2>Connectez-vous</h2>
-      <input
-        type="text"
-        value={inputName}
-        placeholder="Entrez votre prénom"
-        onChange={handleChange}
-        required
-      />
-      <button>Accédez à mon espace</button>
+      <div className="logo-container">
+        <h1 className="title">CRAZEE</h1>
+        <img className="logo" src={Logo} alt="Logo" />
+        <h1 className="title">BURGER</h1>
+      </div>
+      <div className="welcome-container">
+        <h2>Bienvenue chez nous !</h2>
+        <h3>Connectez-vous</h3>
+      </div>
+      <div className="identifier-container">
+        <input
+          type="text"
+          value={inputName}
+          placeholder="Entrez votre prénom"
+          onChange={handleChange}
+          required
+        />
+        <button>Accédez à mon espace</button>
+      </div>
     </LoginFormStyled>
   );
 }
 
 const LoginFormStyled = styled.form`
-  background-color: brown; // a transformer en image
-  background-image 
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: "Amatic SC", cursive;
+  .logo-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: ${theme.fonts.P6};
+    h1 {
+      font-size: ${theme.fonts.P6};
+      color: ${theme.colors.primary};
+    }
+    .logo {
+      height: 40%;
+      width: 40%;
+    }
+  }
+
+  .welcome-container {
+    margin: -4em;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    color: ${theme.colors.white};
+    h2 {
+      font-size: ${theme.fonts.P4};
+    }
+    h3 {
+      margin-top: ${theme.spacing.md};
+      font-size: ${theme.fonts.P3};
+    }
+  }
+  .identifier-container {
+    margin: ${theme.spacing.xl};
+    display: flex;
+    flex-direction: column;
+    margin-bottom: ${theme.spacing.xxs};
+    input {
+      border-radius: ${theme.borderRadius.round};
+    }
+    button {
+      border-radius: ${theme.borderRadius.round};
+    }
+  }
 `;
--- src/components/pages/login/LoginPage.js
+++ src/components/pages/login/LoginPage.js
@@ -1,5 +1,32 @@
 import LoginForm from "./LoginForm";
+import styled from "styled-components";
+import Background from "../../../images/F03-burger-background.jpg";
 
 export default function LoginPage() {
-  return <LoginForm />;
+  return (
+    <LoginPageStyled>
+      <LoginForm />
+    </LoginPageStyled>
+  );
 }
+const LoginPageStyled = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+
+  ::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: url(${Background});
+    background-position: top;
+    background-size: cover;
+    background-repeat: no-repeat;
+    z-index: -1;
+  }
+`;
