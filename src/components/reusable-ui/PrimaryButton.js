a3601c0cb1e1a0b915220f9f60f26eff2232f821
Ovgodd <c.gromoff@gmail.com>
2023-01-16 18:15:29 +0100

    ♻️ F03 LOGIN > get icon from react icon instead of character ( > )

--- src/components/reusable-ui/PrimaryButton.js
+++ src/components/reusable-ui/PrimaryButton.js
@@ -1,16 +1,20 @@
 import React from "react";
 import styled from "styled-components";
 import { theme } from "../../theme";
+import { IoChevronForward } from "react-icons/io5";
 
 export default function PrimaryButton() {
   return (
     <PrimaryButtonStyled>
-      <button className="btn">Accédez à mon espace &#62;</button>
+      <button className="btn">
+        <span>Accédez à mon espace</span>
+        <IoChevronForward className="icon" />
+      </button>
     </PrimaryButtonStyled>
   );
 }
 const PrimaryButtonStyled = styled.div`
-  button {
+  .btn {
     margin: ${theme.spacing.sm};
     border-radius: ${theme.borderRadius.round};
     border: none;
@@ -22,11 +26,17 @@ const PrimaryButtonStyled = styled.div`
     letter-spacing: 1px;
     padding: 18px 24px;
     width: 440px;
+    text-align: center;
+    justify-items: center;
+    justify-content: center;
     &:active {
       background-color: ${theme.colors.white};
       border-color: ${theme.colors.primary_burger};
       color: ${theme.colors.primary_burger};
       border: 1px solid;
+    }
+    .icon {
+      transform: translateY(20%);
     }
   }
 `;
