f3fe1e6186e1f4927769f3056186ea14bc747d57
Ovgodd <c.gromoff@gmail.com>
2023-01-19 14:41:45 +0100

    ✨ F04: ORDER > refreshpage onlick burgerlogo

--- src/components/pages/order/navbar/NavBar.js
+++ src/components/pages/order/navbar/NavBar.js
@@ -1,13 +1,18 @@
 import React from "react";
 import styled from "styled-components";
-import { theme } from "../../../../theme";
 import BurgerLogo from "../../../reusable-ui/BurgerLogo";
 import RightSide from "./RightSide";
 
 export default function NavBar() {
+  const refreshPage = () => {
+    console.log("refreshing");
+    window.location.reload();
+  };
   return (
     <NavBarStyled>
-      <BurgerLogo className="logo" />
+      <div className="logo" onClick={refreshPage}>
+        <BurgerLogo />
+      </div>
       <RightSide />
     </NavBarStyled>
   );
@@ -17,11 +22,13 @@ const NavBarStyled = styled.div`
   flex-direction: row;
   background: white;
   justify-content: space-between;
+  align-items: center;
   border-radius: 15px 15px 0px 0px;
   max-height: 94px;
   .logo {
     transform: scale(0.4);
     transform-origin: center left;
     margin-left: 30px;
+    cursor: pointer;
   }
 `;
