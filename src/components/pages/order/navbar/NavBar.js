beabb2c0374faa189afafbe31b5ae9309e676560
Ovgodd <c.gromoff@gmail.com>
2023-01-19 12:20:10 +0100

    ✨ F04: ORDER >rightside layout

--- src/components/pages/order/navbar/NavBar.js
+++ src/components/pages/order/navbar/NavBar.js
@@ -2,11 +2,13 @@ import React from "react";
 import styled from "styled-components";
 import { theme } from "../../../../theme";
 import BurgerLogo from "../../../reusable-ui/BurgerLogo";
+import RightSide from "./RightSide";
 
 export default function NavBar() {
   return (
     <NavBarStyled>
       <BurgerLogo className="logo" />
+      <RightSide />
     </NavBarStyled>
   );
 }
@@ -21,34 +23,5 @@ const NavBarStyled = styled.div`
     transform: scale(0.4);
     transform-origin: center left;
     margin-left: 30px;
-  }
-  .right-side {
-    display: flex;
-    flex-flow: column wrap;
-    align-items: center;
-    justify-content: space-around;
-    align-content: space-around;
-    margin-right: 70px;
-    background-color: purple;
-    .welcome-elements {
-      background: green;
-    }
-    .welcome-text {
-      font-family: "Open Sans", sans-serif;
-      font-size: ${theme.fonts.P0};
-      right: 0;
-    }
-    .link {
-      color: red;
-      font-size: ${theme.fonts.P0};
-      margin-bottom: 50px;
-    }
-
-    .icon {
-      height: 36px;
-      width: 36px;
-      color: ${theme.colors.greyLight};
-      margin-bottom: 30px;
-    }
   }
 `;
--- src/components/pages/order/navbar/RightSide.js
+++ src/components/pages/order/navbar/RightSide.js
@@ -1,8 +1,49 @@
 import React from "react";
 import { BsPersonCircle } from "react-icons/bs";
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import { theme } from "../../../../theme";
 
 export default function RightSide() {
   const { username } = useParams();
-  return <div>RightSide</div>;
+  return (
+    <RightSideStyled>
+      <div className="welcome-container">
+        <h1 className="welcome-text">Hey, {username}</h1>
+        <a className="link" href="/">
+          Se déconnecter
+        </a>
+      </div>
+      <BsPersonCircle className="icon" />
+    </RightSideStyled>
+  );
 }
+const RightSideStyled = styled.div`
+  display: flex;
+  flex-flow: column wrap;
+  align-items: center;
+  justify-content: space-around;
+  align-content: space-around;
+  margin-right: 70px;
+  background-color: purple;
+  .welcome-container {
+    background: green;
+  }
+  .welcome-text {
+    font-family: "Open Sans", sans-serif;
+    color: ${theme.colors.greyBlue};
+    font-size: ${theme.fonts.P0};
+    right: 0;
+  }
+  .link {
+    color: ${theme.colors.greyBlue};
+    font-size: ${theme.fonts.P0};
+    margin-bottom: 50px;
+  }
+
+  .icon {
+    height: 36px;
+    width: 36px;
+    color: ${theme.colors.greyBlue};
+  }
+`;
