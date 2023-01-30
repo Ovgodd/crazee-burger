3bce3bdd102c68d1481b8688fe477b24c8eed7f4
Ovgodd <c.gromoff@gmail.com>
2023-01-30 16:46:57 +0100

    💄 F05: MENU > add hover and active smooth transition to the button

--- src/components/reusable-ui/Product.js
+++ src/components/reusable-ui/Product.js
@@ -7,9 +7,7 @@ export default function Product({ title, image, price }) {
       <div className="interact-container">
         <h1>{title}</h1>
         <div className="interacter">
-          <div className="price">
-            <span>{price}</span>
-          </div>
+          <span>{price}</span>
           <button>Ajouter</button>
         </div>
       </div>
@@ -34,14 +32,7 @@ const ProductStyled = styled.div`
     object-fit: contain;
     object-position: center;
   }
-  h1 {
-    white-space: nowrap;
-    overflow: hidden;
-    width: 200px;
-    margin-bottom: 5px;
-    text-overflow: ellipsis;
-    font-family: "Amatic SC", cursive;
-  }
+
   .interact-container {
     display: flex;
     flex-direction: column;
@@ -49,34 +40,51 @@ const ProductStyled = styled.div`
     justify-content: space-between;
     width: 200px;
     height: 110px;
-
+    h1 {
+      white-space: nowrap;
+      overflow: hidden;
+      width: 200px;
+      margin-bottom: 5px;
+      text-overflow: ellipsis;
+      font-family: "Amatic SC", cursive;
+    }
     .interacter {
       display: flex;
       justify-content: space-between;
       align-items: center;
       width: 190px;
-    }
-  }
-  span {
-    color: ${theme.colors.primary};
-    font-weight: ${theme.weights.light};
-  }
-  button {
-    border-radius: ${theme.borderRadius.round};
-    border: none;
-    background-color: ${theme.colors.primary};
-    color: ${theme.colors.white};
-    font-size: ${theme.fonts.XS};
-    font-weight: ${theme.weights.light};
-    color: ${theme.colors.white};
-    width: 95px;
-    height: 38px;
+      span {
+        color: ${theme.colors.primary};
+        font-weight: ${theme.weights.regular};
+      }
+      button {
+        border-radius: ${theme.borderRadius.round};
+        border: none;
+        background-color: ${theme.colors.primary};
+        color: ${theme.colors.white};
+        font-size: ${theme.fonts.XS};
+        font-weight: ${theme.weights.bold};
+        color: ${theme.colors.white};
+        width: 95px;
+        height: 38px;
 
-    &:active {
-      background-color: ${theme.colors.white};
-      border-color: ${theme.colors.primary};
-      color: ${theme.colors.primary};
-      border: 1px solid;
+        &:hover {
+          background-color: ${theme.colors.white};
+          border-color: ${theme.colors.primary};
+          color: ${theme.colors.primary};
+          border: 1px solid;
+          transition: 0.3s;
+          cursor: pointer;
+        }
+        &:active {
+          background-color: ${theme.colors.primary};
+          border-color: ${theme.colors.white};
+          color: ${theme.colors.white};
+          border: 1px solid;
+          transition: 0.3s;
+          cursor: pointer;
+        }
+      }
     }
   }
 `;
