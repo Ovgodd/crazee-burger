aedb63c2fe7e09362c8f30713a6b7ccb1c95ef50
Ovgodd <c.gromoff@gmail.com>
2023-01-30 18:18:15 +0100

    ✨ F05: MENU > Add Basket layout

--- src/components/pages/order/Main.js
+++ src/components/pages/order/Main.js
@@ -6,6 +6,7 @@ import MenuStyled from "../../pages/order/navbar/Menu";
 export default function Main() {
   return (
     <MainStyled>
+      {/* <div className="basket">Basket</div> */}
       <MenuStyled />
     </MainStyled>
   );
@@ -17,4 +18,10 @@ const MainStyled = styled.div`
   background: ${theme.colors.background_white};
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
   overflow-y: scroll;
+
+  /* display: grid;
+  grid-template-columns: 25% 1fr;
+  .basket {
+    background: pink;
+  } */
 `;
