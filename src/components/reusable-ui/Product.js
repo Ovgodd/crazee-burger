d964a20f8bc9a71056c89e10afd9489528cb54ec
Ovgodd <c.gromoff@gmail.com>
2023-01-30 15:35:00 +0100

    💄 F05: MENU > Finalyse product global UI

--- src/components/reusable-ui/Product.js
+++ src/components/reusable-ui/Product.js
@@ -6,11 +6,11 @@ export default function Product() {
   return (
     <ProductStyled>
       <img src="../../images/burger3.png" alt="burger" />
-      <div>
-        <h1 className="overflow">meal name overflow point</h1>
-        <div className="interact-container">
+      <div className="interact-container">
+        <h1>Vegan Burger dqdqdzqd</h1>
+        <div className="interacter">
           <div className="price">
-            <span>price</span>
+            <span>9000</span>
           </div>
           <button>Ajouter</button>
         </div>
@@ -28,36 +28,51 @@ const ProductStyled = styled.div`
   border-radius: ${theme.borderRadius.extraRound};
   display: flex;
   flex-direction: column;
+  background: ${theme.colors.white};
+
   img {
     width: 200px;
     height: 145px;
+    object-fit: contain;
+    object-position: center;
   }
   h1 {
     white-space: nowrap;
     overflow: hidden;
     width: 200px;
+    margin: 15px;
     text-overflow: ellipsis;
     font-family: "Amatic SC", cursive;
   }
   .interact-container {
     display: flex;
+    flex-direction: column;
     align-items: center;
     justify-content: space-between;
+    padding: 0px 5px 5px;
+    width: 200px;
+    height: 110px;
+    .interacter {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      width: 200px;
+    }
   }
   span {
     color: ${theme.colors.primary};
+    font-weight: ${theme.weights.light};
   }
   button {
-    background: red;
     border-radius: ${theme.borderRadius.round};
     border: none;
     background-color: ${theme.colors.primary};
     color: ${theme.colors.white};
-    font-size: ${theme.fonts.P0};
+    font-size: ${theme.fonts.XS};
     font-weight: ${theme.weights.light};
     color: ${theme.colors.white};
-    width: 80px;
-    height: 40px;
+    width: 95px;
+    height: 38px;
     &:active {
       background-color: ${theme.colors.white};
       border-color: ${theme.colors.primary};
