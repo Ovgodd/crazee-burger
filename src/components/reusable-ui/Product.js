8b9ee62aa227b13b4cfb2dfc59fca54ac95dd56d
Ovgodd <c.gromoff@gmail.com>
2023-01-30 14:12:38 +0100

    ✨ F05: MENU > stylize "ajouter" boutton

--- src/components/reusable-ui/Product.js
+++ src/components/reusable-ui/Product.js
@@ -7,7 +7,7 @@ export default function Product() {
     <ProductStyled>
       <img src="../../images/burger3.png" alt="burger" />
       <div>
-        <h1 className="overflow">meal name sal</h1>
+        <h1 className="overflow">meal name overflow point</h1>
         <div className="interact-container">
           <div className="price">
             <span>price</span>
@@ -29,20 +29,40 @@ const ProductStyled = styled.div`
   display: flex;
   flex-direction: column;
   img {
-    background: red;
     width: 200px;
     height: 145px;
   }
   h1 {
     white-space: nowrap;
     overflow: hidden;
-    border: 1px solid yellow;
     width: 200px;
     text-overflow: ellipsis;
+    font-family: "Amatic SC", cursive;
   }
   .interact-container {
     display: flex;
     align-items: center;
     justify-content: space-between;
+  }
+  span {
+    color: ${theme.colors.primary};
+  }
+  button {
+    background: red;
+    border-radius: ${theme.borderRadius.round};
+    border: none;
+    background-color: ${theme.colors.primary};
+    color: ${theme.colors.white};
+    font-size: ${theme.fonts.P0};
+    font-weight: ${theme.weights.light};
+    color: ${theme.colors.white};
+    width: 80px;
+    height: 40px;
+    &:active {
+      background-color: ${theme.colors.white};
+      border-color: ${theme.colors.primary};
+      color: ${theme.colors.primary};
+      border: 1px solid;
+    }
   }
 `;
