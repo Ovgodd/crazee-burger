f3cf880bf1857decabdcc7fa36b7d69a441e4bed
Ovgodd <c.gromoff@gmail.com>
2023-01-31 10:30:17 +0100

    ♻️ F05: ORDER > MENU > main from/to menu background css

--- src/components/pages/order/Main/Main.js
+++ src/components/pages/order/Main/Main.js
@@ -15,10 +15,7 @@ const MainStyled = styled.div`
   height: 90vh;
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
-  background: ${theme.colors.background_white};
-  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
   overflow-y: scroll;
-
   /* display: grid;
   grid-template-columns: 25% 1fr;
   .basket {
--- src/components/pages/order/Main/Menu.js
+++ src/components/pages/order/Main/Menu.js
@@ -3,9 +3,10 @@ import styled from "styled-components";
 import Product from "../../../reusable-ui/Product";
 import { fakeMenu2 } from "../../../../fakeData/fakeMenu";
 import { formatPrice } from "../../../../utils/maths";
+import { theme } from "../../../../theme";
 
 export default function Menu() {
-  const [menu] = useState(fakeMenu2);
+  const [menu, setMenu] = useState(fakeMenu2);
   return (
     <MenuStyled>
       {menu.map((element) => (
@@ -25,4 +26,6 @@ const MenuStyled = styled.div`
   padding: 50px 50px 150px;
   grid-template-columns: repeat(4, 1fr);
   grid-row-gap: 60px;
+  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+  background: ${theme.colors.background_white};
 `;
