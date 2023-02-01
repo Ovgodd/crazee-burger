d52ba4b0390a9b6f92afcf1c1da2a7ac0aa30ebd
Ovgodd <c.gromoff@gmail.com>
2023-02-01 15:54:18 +0100

    ✨ F06: MENU > ADMIN > add admin button interact, enable toast

--- src/components/pages/order/navbar/RightSide.js
+++ src/components/pages/order/navbar/RightSide.js
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import NavbarRightSideIncomplet from "./NavbarRightSideIncomplet";
 import ToggleButton from "../../../reusable-ui/ToggleButton";
 import ProfileStyled from "./Profile";
+import { toast } from "react-toastify";
 export default function RightSide({ username }) {
+  const [isAdmin, setIsAdmin] = useState();
+
+  const enableToast = () => {
+    if (!isAdmin) {
+      toast.info("Mode admin activé", {
+        // icon: <FaUserSecret size={30} />,
+        theme: "dark",
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+    setIsAdmin(!isAdmin);
+  };
   return (
     <RightSideStyled>
       <div className="toggle">
         <ToggleButton
+          isChecked={isAdmin}
+          onToggle={enableToast}
           labelIfUnchecked="ACTIVER LE MODE ADMIN"
           labelIfChecked="DÉSACTIVER LE MODE ADMIN"
         />
       </div>
       <ProfileStyled username={username} />
+      <NavbarRightSideIncomplet />
     </RightSideStyled>
   );
 }
