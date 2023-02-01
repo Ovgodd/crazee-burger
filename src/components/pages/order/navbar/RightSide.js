abfa753518b40451e93ec10a4a674da4ac8ab033
Ovgodd <c.gromoff@gmail.com>
2023-02-01 12:15:26 +0100

    🚧 F06: MENU > ADMIN > togglebutton import

--- src/components/pages/order/navbar/RightSide.js
+++ src/components/pages/order/navbar/RightSide.js
@@ -1,9 +1,11 @@
 import React from "react";
 import styled from "styled-components";
+import ToggleButton from "../../../reusable-ui/ToggleButton";
 import ProfileStyled from "./Profile";
 export default function RightSide({ username }) {
   return (
     <RightSideStyled>
+      <ToggleButton />
       <ProfileStyled username={username} />
     </RightSideStyled>
   );
@@ -11,4 +13,7 @@ export default function RightSide({ username }) {
 const RightSideStyled = styled.div`
   display: flex;
   align-items: center;
+  /* background: pink;
+  width: 500px;
+  justify-content: space-between; */
 `;
