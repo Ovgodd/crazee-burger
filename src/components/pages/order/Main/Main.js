f4d158b0d127b8193578be9bc4f6c274020b1ac5
Ovgodd <c.gromoff@gmail.com>
2023-02-07 12:19:15 +0100

    💄 F07: MENU > MAIN > change height from 95vh to 100%

--- src/components/pages/order/Main/Main.js
+++ src/components/pages/order/Main/Main.js
@@ -1,17 +1,19 @@
 import React from "react";
 import styled from "styled-components";
 import { theme } from "../../../../theme";
+import AdminPanel from "./AdminPanel";
 import MenuStyled from "./Menu";
 export default function Main() {
   return (
     <MainStyled>
       {/* <div className="basket">Basket</div> */}
       <MenuStyled />
+      <AdminPanel />
     </MainStyled>
   );
 }
 const MainStyled = styled.div`
-  height: 90vh;
+  height: 100%;
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
   overflow-y: scroll;
