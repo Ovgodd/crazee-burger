c63c2d41696c429dd13a9a11ad5227f3c205ae0b
Ovgodd <c.gromoff@gmail.com>
2023-02-14 13:09:58 +0100

    💄 F07 : MENU  > change grid template parameter in menu.js

--- src/components/pages/order/Main/Menu.js
+++ src/components/pages/order/Main/Menu.js
@@ -24,7 +24,7 @@ const MenuStyled = styled.div`
   justify-items: center;
   padding: 50px 50px 150px;
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
   grid-row-gap: 60px;
   box-shadow: ${theme.shadows.strong};
   background: ${theme.colors.background_white};
