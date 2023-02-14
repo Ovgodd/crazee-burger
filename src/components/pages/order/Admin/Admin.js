117dca7c964638d055ae0a332c9e99c3fc8126b8
Ovgodd <c.gromoff@gmail.com>
2023-02-14 13:34:16 +0100

    🩹 F07: MENU > ADMIN > fix issue while admin was not open when admin mod was enable

--- src/components/pages/order/Admin/Admin.js
+++ src/components/pages/order/Admin/Admin.js
@@ -11,7 +11,7 @@ export default function Admin() {
   return (
     <AdminPanelStyled>
       <AdminTabs />
-      {isCollapsed && <AdminPanel />}
+      {!isCollapsed && <AdminPanel />}
     </AdminPanelStyled>
   );
 }
