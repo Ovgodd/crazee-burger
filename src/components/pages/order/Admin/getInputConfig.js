b8ca2ff49377da22223f2dacff2a059c3b5da070
Ovgodd <c.gromoff@gmail.com>
2023-03-15 16:38:16 +0100

    🏷️ F08 > ADD PRODUCT > add name key to getInputConfig

--- src/components/pages/order/Admin/getInputConfig.js
+++ src/components/pages/order/Admin/getInputConfig.js
@@ -4,17 +4,20 @@ import { MdOutlineEuro } from "react-icons/md";
 export const getInputConfig = () => [
   {
     id: 1,
+    name: "title",
     label: "Nom du produit(ex:Super Burger)",
     Icon: <FaHamburger />,
     type: "text",
   },
   {
     id: 2,
+    name: "imageSource",
     label: "Lien URL d'une image (ex: https://la-photo-de-mon-produit.png)",
     Icon: <BsFillCameraFill />,
     type: "url",
   },
   {
+    name: "price",
     id: 3,
     label: "Prix",
     Icon: <MdOutlineEuro />,
