1ff86ecb2425ab76a3589ebb4d2aa3c40f569afd
Ovgodd <c.gromoff@gmail.com>
2023-03-16 15:49:33 +0100

    💄 F08 > Design system > add theme to admin add product

--- src/components/pages/order/Admin/AdminAddProduct.js
+++ src/components/pages/order/Admin/AdminAddProduct.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { AiOutlineCheckCircle } from "react-icons/ai";
 import styled from "styled-components";
 import OrderContext from "../../../../context/OrderContext";
+import { theme } from "../../../../theme";
 import SecondaryAddButton from "../../../reusable-ui/SecondaryAddButton";
 
 export default function AdminAddProduct({ onClick }) {
@@ -25,6 +26,37 @@ export default function AdminAddProduct({ onClick }) {
   );
 }
 const AdminAddProductStyled = styled.div`
+  .add-new-product {
+    padding: 10px 29px 9px 29px;
+    position: absolute;
+    background: #60bd4f;
+    border: 1px solid #60bd4f;
+    border-radius: 5px;
+    width: 275px;
+    height: 34px;
+    bottom: 0;
+    font-size: 12px;
+    font-family: "Arial";
+    font-style: normal;
+    font-weight: 700;
+    left: 235px;
+    &:hover {
+      background-color: ${theme.colors.white};
+      border-color: ${theme.colors.success};
+      color: ${theme.colors.success};
+      border: 1px solid;
+      transition: 0.3s;
+      cursor: pointer;
+    }
+    &:active {
+      background-color: ${theme.colors.success};
+      border-color: ${theme.colors.white};
+      color: ${theme.colors.white};
+      transition: 0.3s;
+
+      cursor: pointer;
+    }
+  }
   .success-span {
     display: flex;
     flex-direction: row;
