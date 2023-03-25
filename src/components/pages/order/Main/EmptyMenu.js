59f28f03d08857248a28665f6aa7f32a3cb46e0d
Ovgodd <c.gromoff@gmail.com>
2023-03-25 10:38:41 +0100

    ♻️ F08 > Main > EmptyMenu delete useless div and rename className

--- src/components/pages/order/Main/EmptyMenu.js
+++ src/components/pages/order/Main/EmptyMenu.js
@@ -6,15 +6,15 @@ import SecondaryButton from "../../../reusable-ui/SecondaryButton";
 export default function EmptyMenu({ onClick }) {
   return (
     <EmptyMenuStyled>
-      <div className="empty-menu">
-        <span className="question">le menu est vide ? </span>
-        <span className="action">cliquez ci dessous pour le réinitialiser</span>
-        <SecondaryButton
-          className="generate"
-          onClick={onClick}
-          label="Générer de nouveaux produits"
-        />
-      </div>
+      <span className="question">le menu est vide ?</span>
+      <span className="generate-message">
+        cliquez-ci dessous pour le réinitialiser
+      </span>
+      <SecondaryButton
+        className="generate-button"
+        onClick={onClick}
+        label="Générer de nouveaux produits"
+      />
     </EmptyMenuStyled>
   );
 }
@@ -24,54 +24,49 @@ const EmptyMenuStyled = styled.div`
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   font-family: "Amatic SC";
   font-style: normal;
   color: #747b91;
-  .empty-menu {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    .question {
-      font-weight: 700;
-      font-size: 36px;
-    }
-    .action {
-      margin-top: 21px;
-      margin-bottom: 31px;
-      font-weight: 400;
-      font-size: 36px;
-    }
-    .generate {
-      font-family: "Arial";
-      font-style: normal;
-      font-weight: 700;
-      font-size: 12px;
-      color: white;
-      background: #ff9f1b;
-      border: 1px solid #ff9f1b;
-      border-radius: 5px;
-      width: 224px;
-      height: 50px;
+  .question {
+    font-weight: 700;
+    font-size: 36px;
+  }
+  .generate-message {
+    margin-top: 21px;
+    margin-bottom: 31px;
+    font-weight: 400;
+    font-size: 36px;
+  }
+  .generate-button {
+    font-family: "Arial";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 12px;
+    color: white;
+    background: #ff9f1b;
+    border: 1px solid #ff9f1b;
+    border-radius: 5px;
+    width: 224px;
+    height: 50px;
 
-      &:hover {
-        background-color: ${theme.colors.white};
-        border-color: ${theme.colors.primary};
-        color: ${theme.colors.primary};
-        border: 1px solid;
-        transition: 0.3s;
-        cursor: pointer;
-      }
-      &:active {
-        background-color: ${theme.colors.primary};
-        border-color: ${theme.colors.white};
-        color: ${theme.colors.white};
-        transition: 0.3s;
+    &:hover {
+      background-color: ${theme.colors.white};
+      border-color: ${theme.colors.primary};
+      color: ${theme.colors.primary};
+      border: 1px solid;
+      transition: 0.3s;
+      cursor: pointer;
+    }
+    &:active {
+      background-color: ${theme.colors.primary};
+      border-color: ${theme.colors.white};
+      color: ${theme.colors.white};
+      transition: 0.3s;
 
-        cursor: pointer;
-      }
+      cursor: pointer;
     }
   }
 `;
