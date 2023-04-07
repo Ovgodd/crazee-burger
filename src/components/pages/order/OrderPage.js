a7101550b18d10d80689f8ea53c0c9a01843631c
Ovgodd <c.gromoff@gmail.com>
2023-04-07 23:12:44 +0200

    🩹 F09 > Card > setIsCardSelected to null when regenerate product

--- src/components/pages/order/OrderPage.js
+++ src/components/pages/order/OrderPage.js
@@ -30,6 +30,7 @@ export default function OrderPage() {
 
   const handleReset = () => {
     setMenuProducts(fakeMenu.MEDIUM);
+    setIsCardSelected(null);
   };
 
   const handleEdit = (id, updatedProductInfo) => {
