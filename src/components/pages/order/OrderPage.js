1f97bd8d7581eae0076600ac960cf8f4306a043a
Ovgodd <c.gromoff@gmail.com>
2023-04-08 13:01:00 +0200

    🩹 F09 > delete > switch to Edit Text when product is deleted

--- src/components/pages/order/OrderPage.js
+++ src/components/pages/order/OrderPage.js
@@ -19,6 +19,7 @@ export default function OrderPage() {
       return menu.id !== id;
     });
     setMenuProducts(updatedMenu);
+    setSelectedTab("edit");
   };
 
   const handleAdd = (productToAdd) => {
