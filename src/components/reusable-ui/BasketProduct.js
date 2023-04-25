178e87f39ecb2fa79072423befd8dfd3ac49a5a3
Ovgodd <c.gromoff@gmail.com>
2023-04-25 16:50:18 +0200

    ✨ F11 > Basket > create BasketProduct Component

---
+++ src/components/reusable-ui/BasketProduct.js
@@ -0,0 +1,7 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function BasketProduct() {
+  return <BasketProductStyled>BasketProduct</BasketProductStyled>;
+}
+const BasketProductStyled = styled.div``;
