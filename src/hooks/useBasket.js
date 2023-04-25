893e928cced1505c08efc771ef75caac37a4c479
Ovgodd <c.gromoff@gmail.com>
2023-04-25 16:49:52 +0200

    ✨ F11 > Basket > create handleAddtoBasket logic with quantity

--- src/hooks/useBasket.js
+++ src/hooks/useBasket.js
@@ -4,12 +4,31 @@ import { useMenu } from "./useMenu";
 
 export const useBasket = () => {
   const [basket, setBasket] = useState([]);
-  const { menuProducts } = useMenu();
+  const { menuProducts, setMenuProducts } = useMenu();
 
   const handleAddToBasket = (e, productToAdd) => {
     e.stopPropagation();
-    console.log(productToAdd);
-    setBasket(deepClone(menuProducts));
+
+    // Recherche l'index du produit dans le panier
+    const productIndex = basket.findIndex(
+      (product) => product.id === productToAdd.id
+    );
+
+    // Quantité à ajouter au produit dans le panier
+    const quantityToAdd = 1;
+
+    // Si le produit est déjà dans le panier, met à jour sa quantité
+    if (productIndex !== -1) {
+      const basketCopy = deepClone(basket);
+      basketCopy[productIndex] = {
+        ...basketCopy[productIndex],
+        quantity: basketCopy[productIndex].quantity + quantityToAdd,
+      };
+      setBasket(basketCopy);
+    } else {
+      // Si le produit n'est pas encore dans le panier, l'ajoute avec la quantité définie
+      setBasket([...basket, { ...productToAdd, quantity: quantityToAdd }]);
+    }
   };
 
   return {
