3a74ad9e5ec80485c84a9a99f2ab9ffef8675052
Ovgodd <c.gromoff@gmail.com>
2023-04-26 08:56:18 +0200

    ✨ F11 > Adding product to basket with increment

--- src/components/pages/order/Basket/Basket.js
+++ src/components/pages/order/Basket/Basket.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import styled from "styled-components";
 import Header from "./Header";
 import Footer from "./Footer";
 import { formatPrice } from "../../../../utils/maths";
 import BasketProducts from "./BasketProducts";
+import OrderContext from "../../../../context/OrderContext";
 
 export default function Basket() {
+  const { basket } = useContext(OrderContext);
   return (
     <BasketStyled>
       <Header label="Total" amountToPay={formatPrice(0)} />
-      <BasketProducts label="votre commande est vide." />
+      <BasketProducts basket={basket} label="votre commande est vide." />
       <Footer label="Codé avec ❤️ et React.JS" />
     </BasketStyled>
   );
