fb34be3f0b56e289175dec03217e64d469e7ef51
Ovgodd <c.gromoff@gmail.com>
2023-04-26 09:53:26 +0200

    ✨ F11 > Basket > add Format price

--- src/components/pages/order/Basket/BasketProducts.js
+++ src/components/pages/order/Basket/BasketProducts.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import { theme } from "../../../../theme";
 import BasketCard from "../../../reusable-ui/BasketCard";
+import ComingSoon from "../../../../images/coming-soon.png";
+import { formatPrice } from "../../../../utils/maths";
 
 export default function BasketProducts({ basket }) {
   return (
@@ -8,9 +10,9 @@ export default function BasketProducts({ basket }) {
       {basket.map(({ id, imageSource, title, price, quantity }) => (
         <BasketCard
           key={id}
-          image={imageSource}
           title={title}
-          price={price}
+          image={imageSource ? imageSource : ComingSoon}
+          price={formatPrice(price)}
           quantity={quantity}
         />
       ))}
@@ -20,9 +22,10 @@ export default function BasketProducts({ basket }) {
 const BasketProductsStyled = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
   flex: 1;
+  overflow-y: scroll;
   box-shadow: inset 0px 0px 20px rgba(0, 0, 0, 0.2);
   background: ${theme.colors.background_white};
 `;
