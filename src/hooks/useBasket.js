6d65ddf8c8f051c55de014b2da0e98ac912e3646
Ovgodd <c.gromoff@gmail.com>
2023-04-28 18:34:50 +0200

    ✨ F12 > Basket > edit basket product in live

--- src/hooks/useBasket.js
+++ src/hooks/useBasket.js
@@ -53,12 +53,22 @@ export const useBasket = () => {
     basketMenuCopy[productToEdit] = productBeingEdited;
     setBasket(basketMenuCopy);
   };
+  //change element info in basket
+  const handleEditInBasket = (productBeingEdited) => {
+    const basketMenuCopy = deepClone(basket);
+
+    const productToEdit = findIndex(productBeingEdited.id, basket);
+
+    basketMenuCopy[productToEdit] = productBeingEdited;
+    setBasket(basketMenuCopy);
+  };
 
   return {
     basket,
     setBasket,
     handleAddToBasket,
     handleDeleteToBasket,
+    handleEditInBasket,
     handleEditInBasket,
   };
 };
