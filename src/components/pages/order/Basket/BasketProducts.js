1408fad7ebf56958e00d597d5e599daaf5f252af
Ovgodd <c.gromoff@gmail.com>
2023-04-30 12:06:21 +0200

    ♻️ F11 > Basket > refacto basket total calculation

--- src/components/pages/order/Basket/BasketProducts.js
+++ src/components/pages/order/Basket/BasketProducts.js
@@ -3,12 +3,11 @@ import { theme } from "../../../../theme";
 import BasketCard from "../../../reusable-ui/BasketCard";
 import ComingSoon from "../../../../images/coming-soon.png";
 import { formatPrice } from "../../../../utils/maths";
+import OrderContext from "../../../../context/OrderContext";
+import { useContext } from "react";
 
-export default function BasketProducts({
-  basket,
-  isAdmin,
-  handleDeleteToBasket,
-}) {
+export default function BasketProducts() {
+  const { basket, isAdmin, handleDeleteToBasket } = useContext(OrderContext);
   const handleOnDelete = (id) => {
     handleDeleteToBasket(id);
   };
