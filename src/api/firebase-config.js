6e59972e807068d077e84e68c2c3e859b9e11c67
Ovgodd <c.gromoff@gmail.com>
2023-06-13 13:31:12 +0200

    ✨ F13 > passage des config firebase en variable d'environnement

--- src/api/firebase-config.js
+++ src/api/firebase-config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDIufh1yrcyXiE3i-7McworlBBUcjWOwB4",
-  authDomain: "crazee-burger-project-9e954.firebaseapp.com",
-  projectId: "crazee-burger-project-9e954",
-  storageBucket: "crazee-burger-project-9e954.appspot.com",
-  messagingSenderId: "702239542490",
-  appId: "1:702239542490:web:b640c9846513c4dccde9e3",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
