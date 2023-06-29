c5ab10e9d7ae297e76d2e4fa5e1cc06fdec9c393
Ovgodd <c.gromoff@gmail.com>
2023-06-29 10:05:46 +0200

    🚧 F14: ANIMATIONS > basket > totalprice > Amination layout

--- src/components/reusable-ui/CasinoEffects.js
+++ src/components/reusable-ui/CasinoEffects.js
@@ -1,9 +1,35 @@
 import React from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import styled from "styled-components";
 
 export default function CasinoEffects({ count, className }) {
   return (
-    <div>
-      <span className={className}>{count}</span>
-    </div>
+    <CasinoEffectsStyled>
+      <TransitionGroup>
+        <CSSTransition classNames={"count-animated"} timeout={5000} key={count}>
+          <span className={className}>{count}</span>
+        </CSSTransition>
+      </TransitionGroup>
+    </CasinoEffectsStyled>
   );
 }
+const CasinoEffectsStyled = styled.div`
+  .count-animated-enter {
+    background: green;
+  }
+  .count-animated-enter-active {
+    background: blue;
+    transition: 2s;
+  }
+  .count-animated-enter-done {
+    background: pink;
+  }
+
+  .count-animated-exit {
+    background: yellow;
+  }
+  .count-animated-exit-active {
+    background: red;
+    transition: 2s;
+  }
+`;
